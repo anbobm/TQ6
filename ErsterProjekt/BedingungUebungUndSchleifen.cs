c8330aabc1c7a11d7a621cf83a05c1b2f0bbf0b8
Odin2325 <nico.arevalo2325@gmail.com>
2025-12-17 15:57:32 +0100

    HausaufgabeSchleifen

---
+++ ErsterProjekt/BedingungUebungUndSchleifen.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ErsterProjekt
+{
+    class BedingungUebungUndSchleifen
+    {
+        static void Main()
+        {
+
+            //string permission = "Admin|Manager";
+            //int level = 55;
+
+            //string nachricht = "";
+
+            //if (permission.Contains("Admin") && level > 55)
+            //    nachricht = "Welcome, Super Admin user.";
+            //else if (permission.Contains("Admin"))
+            //    nachricht = "Welcome, Admin user.";
+            //else if (permission.Contains("Manager") && level >= 20)
+            //    nachricht = "Contact an Admin for access.";
+            //else
+            //    nachricht = "You do not have sufficient privileges";
+
+            //Console.WriteLine(nachricht);
+
+
+            //string benutzername = "Bob123";
+
+            //Console.WriteLine("Geben Sie bitte Ihr neues Passwort ein:");
+            //string? passwort = Console.ReadLine();
+
+            //while (passwort.Length < 10)
+            //{
+            //    Console.WriteLine("Versuchen Sie es bitte nochmals. Das Passwort muss mind. 10 Zeichen enthalten.");
+            //    passwort = Console.ReadLine();
+            //}
+
+            //Console.WriteLine($"Dein Konto wurde erstellt. " +
+            //    $"Sie haben folgende Zugangsdaten definiert:" +
+            //    $"\nBenutzername: {benutzername}\n" +
+            //    $"Passwort: {passwort}");
+
+
+            //int zahl = 1;
+
+            //while (zahl < 100)
+            //{
+            //    Console.WriteLine("Die Aktuelle Zahl ist: " + zahl);
+
+            //    if (Console.ReadLine() == "quit")
+            //    {
+            //        Console.WriteLine("Die Schleife wird terminiert!");
+            //        break;
+            //    }
+            //}
+
+            //int zahl = 1;
+
+            //while (zahl < 100)
+            //{
+            //    zahl += 10;
+
+            //    if (zahl > 30 && zahl < 50)
+            //    {
+            //        Console.WriteLine("Diese Zahlen moegen wir nicht!");
+            //        continue;
+            //    }
+
+            //    Console.WriteLine("Die Aktuelle Zahl ist: " + zahl);
+            //}
+            //Console.WriteLine("Schleife Vorbei!");
+
+            //HA:
+            //Es soll einen PAP erstellt werden und dannach ein programm geschrieben.
+            //Ein Benutzer soll einen Konto erstellen.
+            //Es gelten folgende regeln:
+            //1. Das Benutzername muss mind. 5 Zeichen lang sein.
+            //2. Das Passwort muss mind. 10 Zeichen lang sein und mit das buchstabe 'A' beginnen.
+            //Sobald eine von diese Faelle nicht stimment, dann soll der Benutzer erneut die Daten eingeben.
+            //Hinweis 1 schleife fuer Benutzername 1 schleife fuer passwort
+
+        }
+    }
+}
--- ErsterProjekt/IfBedingungen.cs
+++ ErsterProjekt/IfBedingungen.cs
@@ -1,111 +1,112 @@
-﻿namespace ErsterProjekt
-{
-    internal class IfBedingungen
-    {
-        static void Main()
-        {
-            Console.WriteLine("Bedingungen Uebungen!");
+﻿//namespace ErsterProjekt
+//{
+//    internal class IfBedingungen
+//    {
+//        static void Main()
+//        {
+//            Console.WriteLine("Bedingungen Uebungen!");
 
-            // Aufgabe 1
-            //Der Benutzer gibt eine Punktzahl(0–100) ein.
-            //Das Programm soll anhand der Punktzahl eine Note ausgeben:
-            //90–100 → „Sehr gut“
-            //75–89 → „Gut“
-            //60–74 → „Befriedigend“
-            //50–59 → „Ausreichend“
-            //0–49 → „Nicht bestanden“
-            //Alle anderen Werte → „Ungültige Eingabe“
+//            // Aufgabe 1
+//            //Der Benutzer gibt eine Punktzahl(0–100) ein.
+//            //Das Programm soll anhand der Punktzahl eine Note ausgeben:
+//            //90–100 → „Sehr gut“
+//            //75–89 → „Gut“
+//            //60–74 → „Befriedigend“
+//            //50–59 → „Ausreichend“
+//            //0–49 → „Nicht bestanden“
+//            //Alle anderen Werte → „Ungültige Eingabe“
 
-            double punkte;
-            Console.WriteLine("Geben Sie ihre Punktzahl ein:");
-            punkte = Convert.ToDouble(Console.ReadLine());
-
-
-            //Loesung 1
-            //if (punkte < 0 || punkte > 100)
-            //{
-            //    Console.WriteLine("Ungültige Eingabe");
-            //}
-            //else
-            //{
-            //    if (punkte < 100)
-            //    {
-            //        if (punkte < 89)
-            //        {
-            //            if (punkte < 74)
-            //            {
-            //                if (punkte > 49)
-            //                {
-            //                    Console.WriteLine("Ausreichend");
-            //                }
-            //                else
-            //                {
-            //                    Console.WriteLine("Nicht Bestanden!");
-            //                }
-            //            }
-            //            else
-            //            {
-            //                Console.WriteLine("Gut");
-            //            }
-            //        }
-            //        else
-            //        {
-            //            Console.WriteLine("Sehr gut");
-            //        }
-            //    }
-            //}
+//            double punkte;
+//            Console.WriteLine("Geben Sie ihre Punktzahl ein:");
+//            punkte = Convert.ToDouble(Console.ReadLine());
 
 
-            //Loesung 2
-            if (punkte < 0 || punkte > 100)
-            {
-                Console.WriteLine("Ungültige Eingabe");
-            }
-            else if (punkte >= 90)
-            {
-                Console.WriteLine("Sehr gut");
-            }
-            else if (punkte >= 75)
-            {
-                Console.WriteLine("Gut");
-            }
-            else if (punkte >= 60)
-            {
-                Console.WriteLine("Befriedigend");
-            }
-            else if (punkte >= 50)
-            {
-                Console.WriteLine("Ausreichend");
-            }
-            else
-            {
-                Console.WriteLine("Nicht Bestanden!");
-            }
-
-            //Übung 2: Console.Write("Bitte Alter eingeben: ");
-
-            int alter = Convert.ToInt32(Console.ReadLine());
-
-            if (alter < 0 || alter > 130)
-            {
-                Console.WriteLine("Ungültige Eingabe");
-            }
-            else if (alter >= 65)
-            {
-                Console.WriteLine("Rentner");
-            }
-            else if (alter >= 18)
-            {
-                Console.WriteLine("Erwachsen");
-            }
-
-            else
-            {
-                Console.WriteLine("Minderjährig");
-            }
+//            //Loesung 1
+//            //if (punkte < 0 || punkte > 100)
+//            //{
+//            //    Console.WriteLine("Ungültige Eingabe");
+//            //}
+//            //else
+//            //{
+//            //    if (punkte < 100)
+//            //    {
+//            //        if (punkte < 89)
+//            //        {
+//            //            if (punkte < 74)
+//            //            {
+//            //                if (punkte > 49)
+//            //                {
+//            //                    Console.WriteLine("Ausreichend");
+//            //                }
+//            //                else
+//            //                {
+//            //                    Console.WriteLine("Nicht Bestanden!");
+//            //                }
+//            //            }
+//            //            else
+//            //            {
+//            //                Console.WriteLine("Gut");
+//            //            }
+//            //        }
+//            //        else
+//            //        {
+//            //            Console.WriteLine("Sehr gut");
+//            //        }
+//            //    }
+//            //}
 
 
+//            //Loesung 2
+//            if (punkte < 0 || punkte > 100)
+//            {
+//                Console.WriteLine("Ungültige Eingabe");
+//            }
+//            else if (punkte >= 90)
+//            {
+//                Console.WriteLine("Sehr gut");
+//            }
+//            else if (punkte >= 75)
+//            {
+//                Console.WriteLine("Gut");
+//            }
+//            else if (punkte >= 60)
+//            {
+//                Console.WriteLine("Befriedigend");
+//            }
+//            else if (punkte >= 50)
+//            {
+//                Console.WriteLine("Ausreichend");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Nicht Bestanden!");
+//            }
 
-        }
-    }
-}
+//            //Übung 2:
+//            Console.Write("Bitte Alter eingeben: ");
+
+//            int alter = Convert.ToInt32(Console.ReadLine());
+
+//            if (alter < 0 || alter > 130)
+//            {
+//                Console.WriteLine("Ungültige Eingabe");
+//            }
+//            else if (alter >= 65)
+//            {
+//                Console.WriteLine("Rentner");
+//            }
+//            else if (alter >= 18)
+//            {
+//                Console.WriteLine("Erwachsen");
+//            }
+
+//            else
+//            {
+//                Console.WriteLine("Minderjährig");
+//            }
+
+
+
+//        }
+//    }
+//}

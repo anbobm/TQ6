9568a6203756740dbe8dc9cbbfab2362e6c7f4b9
sabasarah2007 <sabafarhan@outlook.de>
2026-01-14 15:57:51 +0100

    taschenrechnererstellt

---
+++ ErsterProjekt/TaschenRechner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ErsterProjekt
+{
+    public class TaschenRechner
+    {
+        /*
+ * Erstelle viele Taschenrechner Methoden.
+ * 1. Eine Methode um 2 Zahlen zu summieren (Addition)
+ * 2. Eine Methode um eine Zahl von eine andere abzuziehen (Subtraktion)
+ * 3. Division und Multiplikation auch programmieren.
+ * Hinweis: Jede Methode brauch 2 parameter zahlen. bspw. zahl1 und zahl2.
+ * 
+ * Weitere Aufgaben:
+ * Erstelle eine Methode Exponent: Diese Methode bekommt als Parameter 2 Zahlen.
+ * Einmal das Exponent und einmal die Zahl. Bpsw: (9,2) => 81 => 9*9
+ * Eine Methode fuer sinus, cosinus und tangenz auch programmieren.
+ * 
+ * Letzte Aufgabe, schreibe Methoden fuer Addition, Subtraktion, und Multiplikation die eine Liste von
+ * int/double werte bekommt und die gesamt summe/subtraktion/produkt berechnet.
+ */
+     public static int sum(int a, int b)
+        {
+            return a + b;
+        }
+
+     public static int sub(int a, int b) 
+        {
+            return a - b;
+        }
+     public static int multi(int a, int b)
+        {
+            return a * b;
+        }
+     public static  int division(int a, int b)
+        {
+            return a / b;
+      
+        
+        }
+   
+    
+    
+    }
+
+}
---
+++ ErsterProjekt/WeinachtAufgabe.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ErsterProjekt
+{
+    internal class WeinachtAufgabe
+    {
+
+
+
+
+        //Index               0123456789 10 11 12 13 14 15
+
+
+        public static void RNASequenz(string rnaSequenz)
+        {
+            Console.WriteLine("RNA - Proteinen Konvertierer\n===============================================");
+
+            if (rnaSequenz == null || rnaSequenz == "")
+            {
+                Console.WriteLine("Leere RNA Sequenz.");
+                return;
+            }
+
+            string proteine = "";
+
+            bool stopGefunden = false;
+
+            int zaehler = 0;
+
+            while (!stopGefunden)
+            {
+                string codon = rnaSequenz.Substring(zaehler, 3);
+                switch (codon)
+                {
+                    case "AUG":
+                        proteine += "Methionin ";
+                        break;
+                    case "UUU" or "UUC":
+                        proteine += "Phenylalanin ";
+                        break;
+                    case "UUA" or "UUG":
+                        proteine += "Leucin ";
+                        break;
+                    case "UCU" or "UCC" or "UCA" or "UCG":
+                        proteine += "Serin ";
+                        break;
+                    case "UAU" or "UAC":
+                        proteine += "Tyrosin ";
+                        break;
+                    case "UGU" or "UGC":
+                        proteine += "Cystein ";
+                        break;
+                    case "UGG":
+                        proteine += "Tryptophan ";
+                        break;
+                    case "UAA" or "UAG" or "UGA":
+                        Console.WriteLine("Stop Sequenz gefunden!");
+                        stopGefunden = true;
+                        break;
+                    default:
+                        Console.WriteLine("Keine RNA Sequenz eingegeben. Achten Sie auf Ihre Eingabe bitte!");
+                        return;
+                }
+                zaehler += 3;
+                if (zaehler + 3 > rnaSequenz.Length)
+                {
+
+                    Console.WriteLine("Keine Stop Sequenz gefunden.");
+                    Console.WriteLine("RNA Sequenz vorbei. Programm wird terminiert.");
+                    break;
+                }
+            }
+
+            Console.WriteLine("Gefundene Proteinen: " + proteine);
+        }
+    }
+}

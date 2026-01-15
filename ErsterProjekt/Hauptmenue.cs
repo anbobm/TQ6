38a35c1e88cb4e0d6c7ddf2dcb7935cad0222e47
JohnnyRiot <meyer799@gmail.com>
2026-01-15 13:14:21 +0100

    Alle methoden mit großen buchstaben

---
+++ ErsterProjekt/Hauptmenue.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ErsterProjekt
+{
+    internal class Hauptmenue
+    {
+        
+        public static void Mainmenue() 
+        {
+            Console.WriteLine("Willkommen in der TQ6");
+            Console.WriteLine("\nHier findest du alle Programme die wir entwickelt haben.");
+            Console.WriteLine("\nWähle ein Programm aus:");
+            Console.WriteLine("\n1.Taschenrechner");
+            Console.WriteLine("\nAuswahl:");
+
+            int wahl;
+
+            try
+            {
+                wahl = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nUngültige Eingabe: " + ex.Message);
+                return;
+            }
+            switch (wahl)
+            {
+                case 1:
+                    Rechnermenue();
+                    break;
+
+                default:
+                    Console.WriteLine("Coming Soon!");
+                    break;
+            }
+        
+        }
+        public static void Rechnermenue()
+        {
+            Console.WriteLine("=== Taschenrechner ===");
+            Console.WriteLine("\nWillkommen beim Taschenrechner");
+            Console.WriteLine("\nWas möchtest du machen?");
+            Console.WriteLine("\n1. Addieren");
+            Console.WriteLine("\n2. Subtrahieren");
+            Console.WriteLine("\n3. Multiplizieren");
+            Console.WriteLine("\n4. Dividieren");
+            Console.WriteLine("\n5. Exponenten");
+            Console.WriteLine("\nAuswahl:");
+
+            int wahl;
+
+            try
+            {
+                wahl = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ungültige Eingabe:" + ex.Message);
+                return;
+            }
+            switch (wahl)
+            {
+                case 1:
+                    Taschenrechner.Addieren();
+                    break;
+                case 2:
+                    Taschenrechner.Subtrahieren();
+                    break;
+                case 3:
+                    Taschenrechner.Multiplizieren();
+                    break;
+                case 4:
+                    Taschenrechner.Dividieren();
+                    break;
+                case 5:
+                    Taschenrechner.Exponent();
+                    break;
+                default:
+                    Console.WriteLine("Coming Soon");
+                    break;
+            }
+        }
+    }
+}
---
+++ ErsterProjekt/Taschenrechner.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ErsterProjekt
+{
+    internal class Taschenrechner
+    {
+        private static double zahl1 = 0;
+        private static double zahl2 = 0;
+        private static double ergebnis;
+
+        public static void Addieren()
+        {
+            Console.WriteLine("=== Addieren ===");
+            Console.WriteLine("\nBitte gib die erste Zahl ein:");
+            try
+            {
+                zahl1 = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine("Ungültige Eingabe:" + ex.Message);
+
+            }
+            Console.WriteLine("Bitte gib die zweite Zahl ein:");
+            try
+            {
+                zahl2 = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ungültige Eingabe:" + ex.Message);
+            }
+            ergebnis = zahl1 + zahl2;
+            Console.WriteLine("\nDas Ergebnis ist:" + ergebnis);
+        }
+
+        public static void Subtrahieren()
+        {
+            Console.WriteLine("=== Subtrahieren ===");
+            Console.WriteLine("\nBitte gib die erste Zahl ein:");
+            try
+            {
+                zahl1 = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ungültige Eingabe:" + ex.Message);
+
+            }
+            Console.WriteLine("Bitte gib die zweite Zahl ein:");
+            try
+            {
+                zahl2 = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ungültige Eingabe:" + ex.Message);
+            }
+            ergebnis = zahl1 - zahl2;
+            Console.WriteLine("\nDas Ergebnis ist " + ergebnis);
+        }
+        public static void Multiplizieren()
+        {
+            Console.WriteLine("=== Multiplizierne ===");
+            Console.WriteLine("\nBitte gib die erste Zahl ein:");
+            try
+            {
+                zahl1 = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ungültige Eingabe:" + ex.Message);
+
+            }
+            Console.WriteLine("Bitte gib die zweite Zahl ein:");
+            try
+            {
+                zahl2 = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ungültige Eingabe:" + ex.Message);
+            }
+            ergebnis = zahl1 * zahl2;
+            Console.WriteLine("\nDas Ergebnis ist " + ergebnis);
+        }
+
+        public static void Dividieren()
+        {
+            Console.WriteLine("=== Dividieren ===");
+            Console.WriteLine("\nBitte gib die erste Zahl ein:");
+            try
+            {
+                zahl1 = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ungültige Eingabe:" + ex.Message);
+
+            }
+            Console.WriteLine("Bitte gib die zweite Zahl ein:");
+            try
+            {
+                zahl2 = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ungültige Eingabe:" + ex.Message);
+            }
+            if (zahl2 != 0)
+            {
+                ergebnis = zahl1 / zahl2;
+                Console.WriteLine("\nDas Ergebnis ist " + ergebnis);
+            }
+            else
+            {
+                Console.WriteLine("\nFehler: Division durch 0 ist nicht erlaubt!");
+            }
+        }
+
+        public static void Exponent()
+        {
+            int zaehler = 0;
+            ergebnis = 1;
+
+            Console.WriteLine("=== Exponenten ===");
+            Console.WriteLine("\nBitte gib die erste Zahl ein:");
+            try
+            {
+                zahl1 = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ungültige Eingabe:" + ex.Message);
+
+            }
+            Console.WriteLine("Bitte gib die zweite Zahl ein:");
+            try
+            {
+                zahl2 = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ungültige Eingabe:" + ex.Message);
+            }
+            for (zaehler = 1; zaehler <= zahl2; zaehler++)
+            {
+                ergebnis = ergebnis * zahl1;
+            }
+            Console.WriteLine("\nDas Ergebnis ist " + ergebnis);
+        }
+
+    }
+}

4b66c10d3bf3dc8bee51cd092a1334cd35ac6018
Ron1977 <ron.rossbach77@gmail.com>
2026-01-16 15:48:09 +0100

    MehrereProjekte

---
+++ Heizungssystem/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Heizungssystem
+{
+    // Heizungsklasse
+    public class Heizung
+    {
+        // Attribute
+        private string marke;
+        private int maxTemperatur;
+        private string typ;
+        private int aktuelleTemperatur;
+        private bool anAusSchaltStatus;
+
+        // Konstruktor
+        public Heizung(string marke, int maxTemperatur, string typ)
+        {
+            this.marke = marke;
+            this.maxTemperatur = maxTemperatur;
+            this.typ = typ;
+            this.aktuelleTemperatur = 0;
+            this.anAusSchaltStatus = false;
+        }
+
+        // Methoden
+        public void anschalten()
+        {
+            anAusSchaltStatus = true;
+            Console.WriteLine("Heizung angeschaltet.");
+        }
+
+        public void ausschalten()
+        {
+            anAusSchaltStatus = false;
+            Console.WriteLine("Heizung ausgeschaltet.");
+        }
+
+        public void Hauserwaermen(int gewuenschteTemp)
+        {
+            if (!anAusSchaltStatus)
+            {
+                Console.WriteLine("Heizung ist aus.");
+                return;
+            }
+
+            if (gewuenschteTemp > maxTemperatur)
+            {
+                aktuelleTemperatur = maxTemperatur;
+            }
+            else
+            {
+                aktuelleTemperatur = gewuenschteTemp;
+            }
+
+            Console.WriteLine("Aktuelle Temperatur: " + aktuelleTemperatur + " Grad");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Objekt erzeugen
+            Heizung heizung = new Heizung("08/15", 80, "Wasser");
+
+            // Heizung benutzen
+            heizung.anschalten();
+            heizung.Hauserwaermen(25);
+            heizung.ausschalten();
+
+            Console.ReadLine();
+        }
+    }
+}

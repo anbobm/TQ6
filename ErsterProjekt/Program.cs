b72b52d825e445271aed10bc93c0f882a4e8e82b
sabasarah2007 <sabafarhan@outlook.de>
2026-01-22 13:05:42 +0100

    änderung an main

--- ErsterProjekt/Program.cs
+++ ErsterProjekt/Program.cs
@@ -4,30 +4,7 @@
    {
       static void Main()
        {
-            Bank superBank = new Bank("Super Bank", "Muenchen1", "Muenchen", "Leopold str.11");
-            superBank.KontoErstellen("Nicolas Arevalo Hoelscher");
-            superBank.KontoErstellen("Hilmar");
-            superBank.KontoErstellen("Hannes");
-            superBank.KontoErstellen("Ron");
-            superBank.KontoErstellen("Saba");
-            superBank.KontoErstellen("Vache");
-            superBank.KontoErstellen("Sascha");
-            superBank.KontoErstellen("Bobby");
-
-            Console.WriteLine("Geben Sie bitte Ihre IBAN ein:");
-            string ibanEingabe = Console.ReadLine();
-            Bankkonto meinKonto = superBank.KontoFindenDurchIBAN(ibanEingabe);
-            meinKonto.Einzahlen(100m);
-            meinKonto.Auszahlen(73m);
-            meinKonto.Einzahlen(97m);
-            meinKonto.Einzahlen(2000m);
-            Console.WriteLine("Geben Sie fuer die Ueberweisung einen Ziel IBAN ein:");
-            string zielIban = Console.ReadLine();
-            Console.WriteLine("Geben Sie den Betrag den Sie Ueberweisen moechten ein:");
-            decimal betrag = decimal.Parse(Console.ReadLine());
-            superBank.UeberweisungDurchfuehren(ibanEingabe, zielIban, betrag);
-            meinKonto.Kontoauszug();
-            superBank.KontoFindenDurchIBAN(zielIban).Kontoauszug();
+          
 
             //Bankkonto nicoKonto = new Bankkonto("Nicolas Arevalo Hoelscher");
             //nicoKonto.Kontodetails();

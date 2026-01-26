d1f0b813a5887c137063ef6bad90a525259bc5c7
sabasarah2007 <sabafarhan@outlook.de>
2026-01-26 13:42:54 +0100

    änderung an program.cs

--- ErsterProjekt/Program.cs
+++ ErsterProjekt/Program.cs
@@ -4,9 +4,9 @@
    {
       static void Main()
        {
-            Bank superBank = new Bank("Super Bank", "Muenchen1", "Muenchen", "Leopold str.11");
-            superBank.BankMenueOeffnen();
-            
+            /*Bank superBank = new Bank("Super Bank", "Muenchen1", "Muenchen", "Leopold str.11");
+            superBank.BankMenueOeffnen();*/
+
 
             //Bankkonto nicoKonto = new Bankkonto("Nicolas Arevalo Hoelscher");
             //nicoKonto.Kontodetails();
@@ -18,13 +18,13 @@
             //nicoKonto.Einzahlen(2000m, pin);
             //nicoKonto.Kontoauszug(pin);
 
-            /*    Schule schule = new Schule();
-                schule.AddSchueler(new Person("Saba", 22)); 
-                schule.AddSchueler(new Person("Anna", 30));
-                schule.AddSchueler(new Person("Tom", 18));
-                schule.SchuelerDetails();
-                Schueler s1 = new Schueler("Saba", 43, "12A");
-                s1.Ausgabe();*/
+            Schule schule = new Schule();
+            schule.AddSchueler(new Person("Saba", 22));
+            schule.AddSchueler(new Person("Anna", 30));
+            schule.AddSchueler(new Person("Tom", 18));
+            schule.SchuelerDetails();
+            Schueler s1 = new Schueler("Saba", 43, "12A");
+            s1.Ausgabe();
 
 
         }

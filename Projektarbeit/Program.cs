17efbe1d02a1faede4688eacc92d9041665f0913
JohnnyRiot <meyer799@gmail.com>
2026-02-05 13:08:21 +0100

    Projekt: Projektarbeit erstellt

---
+++ Projektarbeit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Projektarbeit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

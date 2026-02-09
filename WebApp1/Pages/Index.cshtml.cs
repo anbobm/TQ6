ac1e2e412d96cb8ddf6189ef11f0603039aafdb1
hilmar008 <hilmar008@gmail.com>
2026-02-09 20:37:53 +0100

    HTML-Ordner verschoben und WebApp1 erstellt!

---
+++ WebApp1/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp1.Pages;
+
+public class IndexModel : PageModel
+{
+    public string Uhrzeit { get; set; }
+
+    public List<Todo> Todos { get; set; }
+
+    public void OnGet()
+    {
+        Uhrzeit = DateTime.Now.ToString("HH:mm");
+
+        if (!System.IO.File.Exists("todos.json"))
+        {
+            Todos = new List<Todo>();
+        }
+        else
+        {
+            var file = System.IO.File.ReadAllText("todos.json");
+
+            Todos = JsonSerializer.Deserialize<List<Todo>>(file) ?? new List<Todo>();
+        }
+    }
+}
+
+public class Todo
+{
+    public string Titel { get; set; }
+    public bool Erledigt { get; set; }
+}

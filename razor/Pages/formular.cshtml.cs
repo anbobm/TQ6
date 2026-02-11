de88477355230e31599f252d629968da32e5abbe
sabasarah2007 <sabafarhan@outlook.de>
2026-02-11 15:22:00 +0100

    änderung

--- razor/Pages/formular.cshtml.cs
+++ razor/Pages/formular.cshtml.cs
@@ -7,12 +7,28 @@ namespace razor.Pages
     {
         [BindProperty(SupportsGet = true)]
         public string? Name { get; set; }
+        [BindProperty(SupportsGet = true)] public int? Id { get; set; }
+        public Contact? Contact { get; set; }
+        // Simulierte Datenbank
+       
+        
+       private Dictionary<int, Contact> contacts = new Dictionary<int, Contact>{
+        {1, new Contact() {Name = "Max Mustermann", Email = "max@mustermann.com"}},
+        {2, new Contact() {Name = "Petra Schuster", Email = "petraschuster@gmail.com"}},
+        { 23, new Contact() {Name = "Foo Bar", Email = "foo.bar@einfallslos.org"}},
+    };
+   
 
         public void OnGet()
         {
+            if (Id.HasValue && Contacts.ContainsKey(Id.Value))
+            { 
+                Contact = Contacts[Id.Value]; 
+            }
 
         }
     }
+    public record Contact(int Id, string Name);
 }
 
 

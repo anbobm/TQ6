1caeef993fb3af16945a341cc271050734ae2e93
sabasarah2007 <sabafarhan@outlook.de>
2026-02-12 14:02:20 +0100

    formulardatei wieder gestellt

--- razor/Pages/formular.cshtml.cs
+++ razor/Pages/formular.cshtml.cs
@@ -1,36 +1,19 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace razor.Pages
+public class formularModel : PageModel
 {
-    public class formularModel : PageModel
+    public string formular { get; set; }
+
+    public void OnGet(string name)
     {
-        [BindProperty(SupportsGet = true)]
-        public string? Name { get; set; }
-        [BindProperty(SupportsGet = true)] public int? Id { get; set; }
-        public Contact? Contact { get; set; }
-        // Simulierte Datenbank
-       
-        
-       private Dictionary<int, Contact> contacts = new Dictionary<int, Contact>{
-        {1, new Contact() {Name = "Max Mustermann", Email = "max@mustermann.com"}},
-        {2, new Contact() {Name = "Petra Schuster", Email = "petraschuster@gmail.com"}},
-        { 23, new Contact() {Name = "Foo Bar", Email = "foo.bar@einfallslos.org"}},
-    };
-   
-
-        public void OnGet()
+        if (String.IsNullOrEmpty(name))
         {
-            if (Id.HasValue && Contacts.ContainsKey(Id.Value))
-            { 
-                Contact = Contacts[Id.Value]; 
-            }
-
+            return;
         }
-    }
-    public record Contact(int Id, string Name);
-}
 
+        formular = $"Hallo {name}!";
+    }
+}
 
 
 

9a048418c6681f3837e55886ec0968caa4ef94ae
Andreas <foo@bar.com>
2026-02-17 14:55:09 +0100

    AddBook page zugefügt

---
+++ Kurstage/2026-02-13/BiboApp/Pages/AddBook.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class AddBookModel : PageModel
+{
+    public IActionResult OnPost(string titel, string autor, string isausgeliehen)
+    {
+        var db = new BiboContext();
+
+        var buch = new Buch
+        {
+            Titel = titel,
+            Autor = autor,
+            IsAusgeliehen = isausgeliehen == "on"
+        };
+
+        db.Bücher.Add(buch);
+        db.SaveChanges();
+
+        return RedirectToPage("/ListBooks");
+    }
+}

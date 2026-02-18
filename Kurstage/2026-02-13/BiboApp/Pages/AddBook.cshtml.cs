6adcf6e9753981386635ede05d91508c875677bf
Andreas <foo@bar.com>
2026-02-18 16:01:21 +0100

    IsAusgeliehen bleibt False beim Erstellen eines Buches

--- Kurstage/2026-02-13/BiboApp/Pages/AddBook.cshtml.cs
+++ Kurstage/2026-02-13/BiboApp/Pages/AddBook.cshtml.cs
@@ -3,15 +3,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 public class AddBookModel : PageModel
 {
-    public IActionResult OnPost(string titel, string autor, string isausgeliehen)
+    public IActionResult OnPost(string titel, string autor)
     {
         var db = new BiboContext();
 
         var buch = new Buch
         {
             Titel = titel,
-            Autor = autor,
-            IsAusgeliehen = isausgeliehen == "on"
+            Autor = autor
         };
 
         db.Bücher.Add(buch);

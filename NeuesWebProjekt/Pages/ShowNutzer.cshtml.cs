aade5ba0cf31242ad3216d18dd8ae3db26aa8864
Ron1977 <ron.rossbach77@gmail.com>
2026-02-19 13:02:24 +0100

    ShowNutzerHinzugefügt+AddBookAngepasst

---
+++ NeuesWebProjekt/Pages/ShowNutzer.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BiboApp.Database;
+
+namespace BiboApp.Pages
+{
+    public class ShowNutzerModel : PageModel
+    {
+        private readonly BiboContext _context;
+
+        public Nutzer? Nutzer { get; set; }
+
+        public ShowNutzerModel(BiboContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            Nutzer = await _context.Nutzer
+                                    .Include(n => n.AusgelieheneBücher)
+                                    .FirstOrDefaultAsync(n => n.Id == id);
+
+            if (Nutzer == null)
+                return NotFound();
+
+            return Page();
+        }
+    }
+}
